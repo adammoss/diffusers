1a0f4fdb72f3ee4e821ba22fc9b2e7a5f0fb0c2a
Adam Moss <3430494+adammoss@users.noreply.github.com>
2023-04-11 22:37:57 +0100

    Update train_cmd.py

--- train_cmd.py
+++ train_cmd.py
@@ -516,24 +516,6 @@ def main(args):
                 in_channels=num_channels,
                 out_channels=num_channels,
                 encoder_hid_dim=dataset[0]["parameters"].size()[1],
-                layers_per_block=2,
-                block_out_channels=(128, 128, 256, 256, 512, 512),
-                down_block_types=(
-                    "DownBlock2D",
-                    "DownBlock2D",
-                    "DownBlock2D",
-                    "DownBlock2D",
-                    "AttnDownBlock2D",
-                    "DownBlock2D",
-                ),
-                up_block_types=(
-                    "UpBlock2D",
-                    "AttnUpBlock2D",
-                    "UpBlock2D",
-                    "UpBlock2D",
-                    "UpBlock2D",
-                    "UpBlock2D",
-                ),
             )
         else:
             model = UNetModel(
