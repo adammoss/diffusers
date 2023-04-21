b1089a0ab94d2f6191c01dbe1c43379284d8d6b5
Adam Moss <3430494+adammoss@users.noreply.github.com>
2023-04-21 15:10:01 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,30 +6,34 @@ from pipelines import DDPMConditionPipeline
 from schedulers import RePaintScheduler
 
 
-def generate_samples(model, batch_size, device=None):
+def generate_samples(model, batch_size, device=None, num_inference_steps=None):
     pipeline = DDPMConditionPipeline.from_pretrained(model)
     if device is not None:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         pipeline = pipeline.to(device)
+    if num_inference_steps is None:
+        num_inference_steps = pipeline.scheduler.num_train_timesteps
     images = pipeline(
         batch_size=batch_size,
-        num_inference_steps=pipeline.scheduler.num_train_timesteps,
+        num_inference_steps=num_inference_steps,
         output_type="numpy",
     ).images
     return images
 
 
-def inpaint(model, images, mask, device=None):
+def inpaint(model, images, mask, device=None, num_inference_steps=None):
     pipeline = DDPMConditionPipeline.from_pretrained(model)
     scheduler = RePaintScheduler.from_config(pipeline.scheduler.config)
     pipeline = RePaintPipeline.from_pretrained(model, scheduler=scheduler)
     if device is not None:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         pipeline = pipeline.to(device)
+    if num_inference_steps is None:
+        num_inference_steps = pipeline.scheduler.num_train_timesteps
     images = pipeline(
         image=torch.from_numpy(images),
         mask_image=torch.from_numpy(mask),
-        num_inference_steps=pipeline.scheduler.num_train_timesteps,
+        num_inference_steps=num_inference_steps,
         eta=0.0,
         jump_length=10,
         jump_n_sample=10,
