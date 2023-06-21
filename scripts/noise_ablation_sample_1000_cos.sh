da8d3015da8133b456199ad23b96a49c1c8d28ac
Adam Moss <3430494+adammoss@users.noreply.github.com>
2023-06-21 14:09:49 +0100

    scripts

--- scripts/noise_ablation_sample_1000_cos.sh
+++ scripts/noise_ablation_sample_1000_cos.sh
@@ -1,9 +1,9 @@
 for RUN in 1 2 3
 do
-  python train.py --output_dir="output/ddpm-128-epsilon-1000-cos-run$RUN" --base_channels=64 \
+  python train.py --output_dir="output/ddpm-128-sample-1000-cos-run$RUN" --base_channels=64 \
   --resolution=128 --data_size 13500 --ddpm_beta_schedule="squaredcos_cap_v2"  \
   --dataset_name="Mtot_Nbody_SIMBA" --cache_dir="data" \
-  --train_batch_size=64 --eval_batch_size=16 --num_epochs=200 --prediction_type="epsilon" \
+  --train_batch_size=64 --eval_batch_size=16 --num_epochs=200 --prediction_type="sample" \
   --learning_rate=1e-4 --lr_warmup_steps=500 --mixed_precision=no \
   --checkpointing_steps=10000 --use_ema \
   --ddpm_num_steps=1000 --base_channels=64 \
